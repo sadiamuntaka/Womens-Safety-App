a901d09d76f1b078971d2a9a4332fa2012a21052
sadiamuntaka <sadiamuntaka@gmail.com>
2026-03-18 00:27:16 +0600

    Add files via upload

---
+++ BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.darkness.sparkwomen.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.darkness.sparkwomen.test";
+  public static final String BUILD_TYPE = "debug";
+}
